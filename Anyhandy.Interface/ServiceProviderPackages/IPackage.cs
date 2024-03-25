b8a8f047ed0cb23129fd89fd8f39799ad6525998
HasanImran624 <hasanimran4095009@gmail.com>
2024-03-25 23:37:28 +0500

    master : Adding missing packages

---
+++ Anyhandy.Interface/ServiceProviderPackages/IPackage.cs
@@ -0,0 +1,17 @@
+﻿using Anyhandy.Models.DTOs;
+using Anyhandy.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anyhandy.Interface.Packages
+{
+
+
+    public interface IPackage
+    {
+        Task<List<PackageDetailVM>> GetPackageDetailsByMainCategory(int mainCategoryId);
+    }
+}
---
+++ Anyhandy.Services/ServiceProviderPackages/PackagesService.cs
@@ -0,0 +1,41 @@
+﻿using Anyhandy.DataProvider.EFCore.Context;
+using Anyhandy.DataProvider.EFCore.Models;
+using Anyhandy.Interface.Packages;
+using Anyhandy.Interface.User;
+using Anyhandy.Models.DTOs;
+using Anyhandy.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Anyhandy.Services.Users
+{
+    public class PackagesService : IPackage
+    {
+        public async Task<List<PackageDetailVM>> GetPackageDetailsByMainCategory(int mainCategoryId)
+        {
+            using var userPackageContext = new AnyHandyDBContext<UserPackage>();
+
+            using var userProfileRatingContext = new AnyHandyDBContext<UserProfileRating>();
+            var packages = await userPackageContext.UserPackages
+           .Where(p => p.MainCategoryId == mainCategoryId && p.Active)
+           .Include(p => p.HandymanUser.UserProfileRatingUsers) // Assuming there's a navigation property for the HandymanUser
+           .Select(p => new PackageDetailVM
+           {
+               Title = p.Title,
+               Description = p.Description,
+               Price = p.Price,
+               UserName = p.HandymanUser.FirstName + " " + p.HandymanUser.LastName,
+               Rating = p.HandymanUser.UserProfileRatingUsers
+                            .Where(r => r.UserId == p.HandymanUserId)
+                            .Average(r => r.Rating)
+           })
+           .ToListAsync();
+
+            return (packages);
+        }
+
+    }
+}
